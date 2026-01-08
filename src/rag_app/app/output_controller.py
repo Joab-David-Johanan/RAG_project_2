a9cbc315b0920a1f14ba010e734cecc73312a85f
Joab David Johanan <joabkindle@gmail.com>
2026-01-08 17:58:41 +0100

    udpate graph streaming

--- src/rag_app/app/output_controller.py
+++ src/rag_app/app/output_controller.py
@@ -1,14 +1,15 @@
-import streamlit as st
 from rag_app.ui.build_ui.load_conversation import BuildResultDisplay
-
 
 class OutputController:
     def render(self, user_input: dict, graph):
-        user_message = st.chat_input("Enter your message")
 
-        if user_message:
-            BuildResultDisplay(
-                usecase=user_input["selected_usecase"],
-                graph=graph,
-                user_message=user_message
-            ).display()
+        user_message = user_input.get("user_message")
+
+        if not user_message:
+            return
+
+        BuildResultDisplay(
+            usecase=user_input["selected_usecase"],
+            graph=graph,
+            user_message=user_message
+        ).display()
